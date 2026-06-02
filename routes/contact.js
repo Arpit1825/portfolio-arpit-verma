30b012b96c1b2895ccdd2604272480cdb8650bb8
Arpit1825 <2k24.csaiml1a2413757@gmail.com>
2026-06-02 18:30:26 +0530

    Added MongoDB Atlas and Nodemailer integration

--- routes/contact.js
+++ routes/contact.js
@@ -1,13 +1,34 @@
 const express = require("express");
 const router = express.Router();
-
+const transporter = require("../mailer");
 const Message = require("../models/Message");
 
 router.post("/", async (req, res) => {
   console.log("Request received:", req.body);
-
+  
   try {
     const message = await Message.create(req.body);
+
+     const info = await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to: process.env.EMAIL_USER,
+      subject: `New Portfolio Message: ${req.body.subject}`,
+     html: `
+<div style="font-family: Arial, sans-serif">
+  <h2>📩 New Portfolio Message</h2>
+
+  <p><strong>Name:</strong> ${req.body.name}</p>
+  <p><strong>Email:</strong> ${req.body.email}</p>
+  <p><strong>Subject:</strong> ${req.body.subject}</p>
+
+  <hr>
+
+  <p>${req.body.message}</p>
+</div>
+`,
+    });
+
+    console.log("Mail sent:", info.messageId);
 
     res.status(201).json({
       success: true,
